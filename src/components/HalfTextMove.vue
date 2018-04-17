6eb90a4f224107e75f5834ceecf4a4668fb72158
sakapun <sakapun5th@gmail.com>
2018-04-17 23:34:37 +0900

    半分の文字

---
+++ src/components/HalfTextMove.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="wrap">
+      <div class="hide">hide</div>
+      <span class="half top">
+        <span class="trans">{{text}}</span>
+      </span>
+      <span class="half bottom">
+        <span class="trans">{{text}}</span>
+      </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HalfTextMove",
+      props: {
+          text: {
+            type: String,
+            required: true
+          }
+      }
+    }
+</script>
+
+<style scoped>
+.wrap {
+  position: relative;
+  width: 200px;
+  font-size: 2em;
+}
+.hide {
+  visibility: hidden;
+}
+  .half {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 50%;
+    overflow: hidden;
+  }
+  .half span {
+    height: 100%;
+  }
+  .top {
+    top: 0;
+  }
+  .bottom {
+    bottom: 0;
+    color: red;
+  }
+  .bottom .trans {
+    bottom: 100%;
+    position: absolute;
+    transition: translate 0.7s;
+    transform: translateY(100%)
+  }
+</style>
--- src/stories/second.stories.js
+++ src/stories/second.stories.js
@@ -5,12 +5,17 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import HoverGradient from '../components/HoverGradient.vue';
-
+import HalfTextMove from '../components/HalfTextMove.vue';
 
 storiesOf('HoverGradient', module)
   .add('with text', () => ({
     components: { HoverGradient },
     template: '<div style="width: 300px"><HoverGradient></div>',
+    methods: { action: action('clicked') },
+  }))
+  .add('HalfTextMove', () => ({
+    components: { HalfTextMove },
+    template: '<HalfTextMove text="Hoge" />',
     methods: { action: action('clicked') },
   }))
 /* eslint-enable react/react-in-jsx-scope */
